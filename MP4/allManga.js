4a4d7ab98e3a31b873f09d9264463c354be81655
Ylruhc <osmandawud91@gmail.com>
2025-04-23 01:59:10 +0100

    Update allManga.js

--- MP4/allManga.js
+++ MP4/allManga.js
@@ -125,7 +125,7 @@ async function extractEpisodes(id) {
         console.log(anime)
         const transformedResults = anime.availableEpisodesDetail.sub.map(episode => ({
             href: JSON.stringify({showId:id,translationType:"sub",episodeString:episode}),
-            number: episode
+            number:  parseInt(episode, 10) || 0
         }));
         
         return JSON.stringify(transformedResults);
