3d71ba89aaa2212fd1128687eadf07b3f7309b72
Ylruhc <osmandawud91@gmail.com>
2025-05-05 15:20:25 +0100

    Update allMangaDefault.js

--- Default/allMangaDefault.js
+++ Default/allMangaDefault.js
@@ -270,7 +270,7 @@ async function defaultExtractor(url) {
   }
 // OKRU extractor
 async function okruExtractor(url) {
-  const response = await fetchv2(url)
+  const response = await fetchv2(url,{"User-Agent":"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/134.0.0.0 Safari/537.36"})
   const body = await response.text()
   const match = body.match(/data-options="([^"]*)"/);
   if(match)
