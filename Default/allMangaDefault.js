f1c3313334d82be9ec454dc16e4996144948e5ac
Ylruhc <osmandawud91@gmail.com>
2025-05-21 16:08:13 +0100

    Update allMangaDefault.js

--- Default/allMangaDefault.js
+++ Default/allMangaDefault.js
@@ -188,7 +188,7 @@ async function extractStreamUrl(url) {
           {
             //streams.push("Okru")
             //streams.push(streamUrl)
-            streams.push({title:"okru",streamUrl:streamUrl,headers:{}})
+            streams.push({title:"okru",streamUrl:streamUrl,headers:{"User-Agent":"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/134.0.0.0 Safari/537.36"}})
           }
         }
       } catch{console.error("OK fetch error")}
